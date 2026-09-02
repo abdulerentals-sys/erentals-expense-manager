e10d5b95a141125565e9299ecfe00a2fa855c0bd
abdulerentals-sys <abdulerentals@gmail.com>
2026-09-02 14:21:14 +0530

    Add reimbursement history to expense dashboard

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,4 +1,5 @@
 import SupervisorExpenseDashboard from "../components/SupervisorExpenseDashboard";
+import SupervisorReimbursementHistory from "../components/SupervisorReimbursementHistory";
 import { canViewSection } from "../auth/permissions";
 import { requireDashboardUser } from "../auth/session";
 import { redirect } from "next/navigation";
@@ -8,5 +9,5 @@ export const dynamic = "force-dynamic";
 export default async function ExpensesPage() {
   const user = await requireDashboardUser();
   if (!canViewSection(user.role, "expenses")) redirect("/");
-  return <SupervisorExpenseDashboard user={user} />;
+  return <><SupervisorExpenseDashboard user={user} /><SupervisorReimbursementHistory /></>;
 }
