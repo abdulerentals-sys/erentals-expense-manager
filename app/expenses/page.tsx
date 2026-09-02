3a948e90e82bf36b60f8c67abf4fb00a109dfd15
abdulerentals-sys <abdulerentals@gmail.com>
2026-09-02 14:21:31 +0530

    Apply supervisor expense dashboard styling

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,4 +1,5 @@
 import SupervisorExpenseDashboard from "../components/SupervisorExpenseDashboard";
+import SupervisorExpenseStyles from "../components/SupervisorExpenseStyles";
 import SupervisorReimbursementHistory from "../components/SupervisorReimbursementHistory";
 import { canViewSection } from "../auth/permissions";
 import { requireDashboardUser } from "../auth/session";
@@ -9,5 +10,5 @@ export const dynamic = "force-dynamic";
 export default async function ExpensesPage() {
   const user = await requireDashboardUser();
   if (!canViewSection(user.role, "expenses")) redirect("/");
-  return <><SupervisorExpenseDashboard user={user} /><SupervisorReimbursementHistory /></>;
+  return <><SupervisorExpenseStyles /><SupervisorExpenseDashboard user={user} /><SupervisorReimbursementHistory /></>;
 }
