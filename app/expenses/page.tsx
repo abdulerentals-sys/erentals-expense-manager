0e7126842ac85657aaeca9727c714bb2b6f2f148
abdulerentals-sys <abdulerentals@gmail.com>
2026-09-02 14:21:43 +0530

    Add expense entry action to command centre

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,6 +1,7 @@
 import SupervisorExpenseDashboard from "../components/SupervisorExpenseDashboard";
 import SupervisorExpenseStyles from "../components/SupervisorExpenseStyles";
 import SupervisorReimbursementHistory from "../components/SupervisorReimbursementHistory";
+import ExpenseEntryLink from "../components/ExpenseEntryLink";
 import { canViewSection } from "../auth/permissions";
 import { requireDashboardUser } from "../auth/session";
 import { redirect } from "next/navigation";
@@ -10,5 +11,5 @@ export const dynamic = "force-dynamic";
 export default async function ExpensesPage() {
   const user = await requireDashboardUser();
   if (!canViewSection(user.role, "expenses")) redirect("/");
-  return <><SupervisorExpenseStyles /><SupervisorExpenseDashboard user={user} /><SupervisorReimbursementHistory /></>;
+  return <><SupervisorExpenseStyles /><SupervisorExpenseDashboard user={user} /><ExpenseEntryLink /><SupervisorReimbursementHistory /></>;
 }
